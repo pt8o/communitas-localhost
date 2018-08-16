79a97fcf7e61d6789a49a432bc5f3e6e31d9a2ae
suprko <lucas@peerio.com>
2018-08-16 00:43:14 -0400

    updated Our Work page

---
+++ wp-content/themes/communitas-2018/page-our-work.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Communitas_2018
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<div
+                class="page-headline"
+                style="background-image:url('<?php
+                    $headerImage = get_field('header_image');
+                    if ($headerImage){
+                        echo $headerImage;
+                    } else {
+                        echo esc_url( home_url( '/wp-content/themes/communitas-2018/static/home_newsprint.jpg' ) );
+                    }
+                ?>');"
+            >
+                <div class="headline">
+                    <h2><?php echo get_the_title() ?></h2>
+                </div>
+			</div>
+			
+		<?php		
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+        ?>
+        
+        
+            <div class="card-container">
+                <?php
+                    if (have_rows('areas_of_work')):
+                    while (have_rows('areas_of_work')) : the_row();
+                        $title = get_sub_field('title');
+                        $icon = get_sub_field('icon');
+                        $description = get_sub_field('description');
+                        $url = get_sub_field('link');
+                ?><a href="<?php echo $url; ?>" class="card responsive third"><img class="icon-deco" src="<?php echo $icon; ?>" alt="" /><br/><h4><?php echo $title; ?></h4><p><?php echo $description; ?></p></a><?php
+                    endwhile;
+                    endif;
+                ?>
+            </div>
+        
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
